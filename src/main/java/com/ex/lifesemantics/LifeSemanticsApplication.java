106bba39c83da4541c984f02eb172aeaf3090132
shkimhi <ajfqhksh1@gmail.com>
2023-02-07 04:03:32 +0900

    @ComponentScan(basePackages = {"com.ex.lifesemantics"}) 추가

--- src/main/java/com/ex/lifesemantics/LifeSemanticsApplication.java
+++ src/main/java/com/ex/lifesemantics/LifeSemanticsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.ex.lifesemantics"})
 @MapperScan(basePackages = {"com.ex.lifeSemantics"})
 public class LifeSemanticsApplication {
 
