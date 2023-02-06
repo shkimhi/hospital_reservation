99ae73cc16a0ac0e2d0cec79d1fa8030eb384d32
shkimhi <ajfqhksh1@gmail.com>
2023-02-07 04:21:44 +0900

    @MapperScan(basePackages = {"com.ex"})수정

--- src/main/java/com/ex/lifesemantics/LifeSemanticsApplication.java
+++ src/main/java/com/ex/lifesemantics/LifeSemanticsApplication.java
@@ -3,11 +3,9 @@ package com.ex.lifesemantics;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.ex.lifesemantics"})
-@MapperScan(basePackages = {"com.ex.lifeSemantics"})
+@MapperScan(basePackages = {"com.ex"})
 public class LifeSemanticsApplication {
 
     public static void main(String[] args) {
